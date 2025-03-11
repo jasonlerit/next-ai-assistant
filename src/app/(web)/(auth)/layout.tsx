296084411c805477b1776992ab3a8fa7bf8af210
jasonlerit <jasonlerit077@gmail.com>
2025-03-11 23:08:24 +0800

    feat: use middleware to protect routes

--- src/app/(web)/(auth)/layout.tsx
+++ src/app/(web)/(auth)/layout.tsx
@@ -1,18 +1,3 @@
-"use client"
-
-import { authClient } from "@/lib/auth-client"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  const router = useRouter()
-  const { data, isPending } = authClient.useSession()
-
-  useEffect(() => {
-    if (data !== null && !isPending) {
-      router.push("/dashboard")
-    }
-  }, [data, isPending, router])
-
   return children
 }
--- src/app/(web)/(protected)/layout.tsx
+++ src/app/(web)/(protected)/layout.tsx
@@ -1,18 +1,3 @@
-"use client"
-
-import { authClient } from "@/lib/auth-client"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
-  const router = useRouter()
-  const { data, isPending } = authClient.useSession()
-
-  useEffect(() => {
-    if (data === null && !isPending) {
-      router.push("/sign-in")
-    }
-  }, [data, isPending, router])
-
   return children
 }
