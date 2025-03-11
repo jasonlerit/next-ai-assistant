db8af843b2bb61153272e9b4222726e093d8edbd
jasonlerit <jasonlerit077@gmail.com>
2025-03-11 23:23:36 +0800

    feat: Sign In page

--- src/app/(web)/(auth)/sign-in/page.tsx
+++ src/app/(web)/(auth)/sign-in/page.tsx
@@ -1,19 +1,9 @@
-"use client"
-
-import { Button } from "@/components/ui/button"
-import { authClient } from "@/lib/auth-client"
+import SignInForm from "@/app/(web)/(auth)/sign-in/_components/sign-in-form"
 
 export default function SignIn() {
-  const handleSignIn = async () => {
-    await authClient.signIn.social({
-      provider: "google",
-      callbackURL: "/dashboard",
-    })
-  }
-
   return (
     <main className='w-full h-screen grid place-content-center'>
-      <Button onClick={handleSignIn}>Sign In</Button>
+      <SignInForm />
     </main>
   )
 }
