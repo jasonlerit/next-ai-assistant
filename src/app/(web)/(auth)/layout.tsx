4ad34d3304b8ae180b4c9136a1adf373d8fd79ae
jasonlerit <jasonlerit077@gmail.com>
2025-03-12 20:42:29 +0800

    refactor: check user session in layout

--- src/app/(web)/(auth)/layout.tsx
+++ src/app/(web)/(auth)/layout.tsx
@@ -1,3 +1,18 @@
+"use client"
+
+import { authClient } from "@/lib/auth-client"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+  const { data, isPending } = authClient.useSession()
+
+  useEffect(() => {
+    if (data !== null && !isPending) {
+      router.push("/assistants")
+    }
+  }, [router, data, isPending])
+
   return children
 }
--- src/app/(web)/(protected)/layout.tsx
+++ src/app/(web)/(protected)/layout.tsx
@@ -1,8 +1,22 @@
+"use client"
+
 import { AppSidebar } from "@/components/shared/app-sidebar"
 import { Navbar } from "@/components/shared/navbar"
 import { SidebarProvider } from "@/components/ui/sidebar"
+import { authClient } from "@/lib/auth-client"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+  const { data, isPending } = authClient.useSession()
+
+  useEffect(() => {
+    if (data === null && !isPending) {
+      router.push("/sign-in")
+    }
+  }, [router, data, isPending])
+
   return (
     <SidebarProvider>
       <AppSidebar />
